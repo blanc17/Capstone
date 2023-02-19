2901de64ef4fdf3a486ea1f7997110b130c706f2
Alyssa Blanc <97771623+blanc17@users.noreply.github.com>
2023-02-19 18:42:45 -0500

    Continued working on classes for tools

--- GUI/GUI with classes.py
+++ GUI/GUI with classes.py
@@ -14,9 +14,16 @@ class windows(tk.Tk):
         self.wm_title('AxiDraw Assist')
         #resize the window
         self.wm_state('zoomed')
+
+        #special variables
+        self.medium='pen'
         
         #creating a menu for the main window
         self.make_menu()
+
+        #create a toolbar for the main window
+        self.tools = {}
+        self.make_toolbar()
 
         #create a large generic frame for the main window
         container = tk.Frame(self)
@@ -24,6 +31,7 @@ class windows(tk.Tk):
         
 
     def make_menu(self):
+        
         menu = tk.Menu()
 
         #add a file button
@@ -34,8 +42,21 @@ class windows(tk.Tk):
 
         #add the menu to the window
         self.config(menu=menu)
-        
 
+    def make_toolbar(self):
+        tools = tk.Frame(self)
+        for B in (Pen, Eraser, Circle, Rectangle, Edit):
+            button = B(tools,self)
+            self.tools[B] = button
+        tools.pack(anchor='n',fill='x')
+
+    def set_medium(self, medium):
+        self.medium = medium
+        print(self.medium)
+
+            
+        
+#Classes for file menu
 class New():
     def __init__(self, button):
         button.add_command(label='New', command=self.new_file)
@@ -53,6 +74,31 @@ class Save():
         button.add_command(label='Save', command=self.save_file)
     def save_file(self):
         print('Save file')
+
+#Classes for toolbar menu
+class Pen():
+    def __init__(self, parent, window):
+        pen = tk.Button(parent, text='Pen', command = lambda: window.set_medium('Pen'))
+        pen.pack(side='left',fill='none')
+class Eraser():
+    def __init__(self, parent, window):
+        pen = tk.Button(parent, text='Eraser', command= lambda: window.set_medium('Eraser'))
+        pen.pack(side='left',fill='none')
+class Circle():
+    def __init__(self, parent, window):
+        pen = tk.Button(parent, text='Circle', command = lambda: window.set_medium('Circle'))
+        pen.pack(side='left',fill='none')
+class Rectangle():
+    def __init__(self, parent, window):
+        pen = tk.Button(parent, text='Rectangle', command = lambda: window.set_medium('Rectangle'))
+        pen.pack(side='left',fill='none')
+class Edit():
+    def __init__(self, parent, window):
+        pen = tk.Button(parent, text='Edit', command = lambda: window.set_medium('Edit'))
+        pen.pack(side='left',fill='none')
+        
+
+#Classes for buttons
 
 #run the program
 if __name__ == '__main__':
