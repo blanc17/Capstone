b1127af86ec81f0ff51c01a1b1cd95d142f1585c
Alyssa Blanc <97771623+blanc17@users.noreply.github.com>
2023-03-06 12:00:52 -0500

    Add files via upload

---
+++ Test/test for converting pngs to svgs.py
@@ -0,0 +1,23 @@
+import aspose.words as aw
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+#eimport pypotrace as pypo
+########################################
+
+#Using aspose
+
+# doc = aw.Document()
+# builder = aw.DocumentBuilder(doc)
+
+# file = askopenfilename(
+#             filetypes=[
+#                 ('Scalabale Vector Graphics', '*.svg'),
+#                 ('Images', '*.png *.img *.bmp *.jpeg'),
+#                 ('All Files', '*.*')
+#             ]
+#         )
+
+# shape = builder.insert_image(file)
+# shape.image_data.save("Output.svg")
+
+########################################
+
