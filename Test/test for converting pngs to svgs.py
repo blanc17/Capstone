8011c5a381975f98dc3d67bb95c2d0114b7cde1b
Alyssa Blanc <97771623+blanc17@users.noreply.github.com>
2023-04-09 22:16:19 -0400

    More attempts with aspose (internet version works)

--- Test/test for converting pngs to svgs.py
+++ Test/test for converting pngs to svgs.py
@@ -1,6 +1,32 @@
+# import aspose.words as aw
+# from tkinter.filedialog import askopenfilename, asksaveasfilename
+# import os
+
+# doc = aw.Document()
+# builder = aw.DocumentBuilder(doc)
+
+# file = askopenfilename(
+#             filetypes=[
+#                 ('Scalabale Vector Graphics', '*.svg'),
+#                 ('Images', '*.png *.img *.bmp *.jpeg'),
+#                 ('All Files', '*.*')
+#             ]
+#         )
+# shape = builder.insert_image(file)
+# filename = os.path.realpath(os.path.dirname(__file__)) + '\\' + 'test_aspose.svg'
+# shape.image_data.save(filename)
+
 import aspose.words as aw
-from tkinter.filedialog import askopenfilename, asksaveasfilename
-#eimport pypotrace as pypo
+import os
+
+doc = aw.Document()
+builder = aw.DocumentBuilder(doc)
+path = os.path.realpath(os.path.dirname(__file__)) + '\\'
+shape = builder.insert_image(path + "Myles Morales.png")
+shape.image_data.save(path + "aghagh.svg")
+
+
+#import pypotrace as pypo
 ########################################
 
 #Using aspose
